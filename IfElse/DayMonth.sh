714847eab512847a8523550c7417addbd9977c48
vampire16 <siddheshsalvi525@gmai.com>
2020-02-22 18:10:21 +0530

    MODIFIED : added comments and double quoted for echo statement

--- IfElse/DayMonth.sh
+++ IfElse/DayMonth.sh
@@ -1,17 +1,16 @@
 #!/bin/bash -x
-#takes day and month from the command-line and prints true if day and month is between march 20 to june 20
 
-#checking month and day
-if [ $2 -eq 3 -a $1 -ge 20 -a $1 -le 31 ]     
-then echo true
+#TAKING INPUT FROM COMMAND-LINE $1-DAY, $2-MONTH
+if [ $2 -eq 3 -a $1 -ge 20 -a $1 -le 31 ]
+then echo "true"
 fi
-if [ $2 -eq 4 -a $1 -ge 1 -a $1 -le 30 ]   
-then echo true
+if [ $2 -eq 4 -a $1 -ge 1 -a $1 -le 30 ]
+then echo "true"
 fi
-if [ $2 -eq 5 -a $1 -ge 1 -a $1 -le 31 ]   
-then echo true
+if [ $2 -eq 5 -a $1 -ge 1 -a $1 -le 31 ]
+then echo "true"
 fi
-if [ $2 -eq 6 -a $1 -le 20 -a $1 -ge 1 ]   
-then echo true
-else echo false
+if [ $2 -eq 6 -a $1 -le 20 -a $1 -ge 1 ]
+then echo "true"
+else echo "false"
 fi
