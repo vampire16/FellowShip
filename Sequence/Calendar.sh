7cb5fc13cc35fa5444df1351d82579864cd048c8
vampire16 <siddheshsalvi525@gmai.com>
2020-02-22 16:40:59 +0530

    MODIFIED : added constants

--- Sequence/Calendar.sh
+++ Sequence/Calendar.sh
@@ -1,26 +1,26 @@
 #!/bin/bash -x
 #Write a program that takes a date as input and prints the day of the week that date falls on
 
-#taking command-line input $1, $2, $3 as a date, month, year 
+#COMMAND-LINE INPUT $1,$2,$3
 date=$1
 month=$2
 year=$3
 
 
-yo=$(($y-(14-$m)/12))
+yo=$(($year-(14-$month)/12))
 x=$(($yo+$yo/4-$yo/100+$yo/400))
-mo=$(($m+12*((14-$m)/12)-2))
-dO=$((($d+$x+31*($mo/12))%7))
+mo=$(($month+12*((14-$month)/12)-2))
+dO=$((($date+$x+31*($mo/12))%7))
 
 case "$dO" in
 
-"0") echo "SUNDAY" ;;
-"1") echo "MONDAY" ;;
-"2") echo "TUESDAY" ;;
-"3") echo"WEDNESDAY" ;;
-"4") echo "THURSDAY" ;;
-"5") echo "FRIDAY" ;;
-"*") echo "SATURDAY" ;;
+0) echo "SUNDAY" ;;
+1) echo "MONDAY" ;;
+2) echo "TUESDAY" ;;
+3) echo"WEDNESDAY" ;;
+4) echo "THURSDAY" ;;
+5) echo "FRIDAY" ;;
+*) echo "SATURDAY" 
 esac
 
 
