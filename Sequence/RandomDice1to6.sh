f5de2de3984e660eeee449e36f70aede429184fd
vampire16 <siddheshsalvi525@gmai.com>
2020-02-22 16:52:39 +0530

    MODIFIED : added double quotes

--- Sequence/RandomDice1to6.sh
+++ Sequence/RandomDice1to6.sh
@@ -2,6 +2,5 @@
 #Use Random to get Dice Number between 1 to 6
 
 #store the output in variable and print the result
-randomCheck=$((RANDOM%6 + 1));      #((RANDOM)) it is a function 
-echo $randomCheck
-	
+randomCheck=$((RANDOM%6 + 1));
+echo "Random Dice number : $randomCheck"
