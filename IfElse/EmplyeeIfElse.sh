6f23151acf87255b7ff750707bbf1df889e49d33
vampire16 <siddheshsalvi525@gmai.com>
2020-02-22 18:20:16 +0530

    MODIFIED : added comments and changed echo statement

--- IfElse/EmplyeeIfElse.sh
+++ IfElse/EmplyeeIfElse.sh
@@ -1,14 +1,14 @@
 #! /bin/bash -x
 #check Employee is present or absent
 
-#take a value for present variable
+#CONSTANT VARIABLES
 isPresent=1;
+
 randomCheck=$((RANDOM%2))
 
-#check the present variable with random number
 if [ $isPresent -eq $randomCheck ];
 then
-	echo "employee present";
+   echo "Employee present";
 else
-   echo "employee absent";
+   echo "Employee absent";
 fi
