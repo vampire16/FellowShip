6ca0a8e6f949887ba6ad765193cfc3f7008552c6
vampire16 <siddheshsalvi525@gmai.com>
2020-02-22 17:12:18 +0530

    MODIFIED : added double quotes and comment

--- Sequence/TwoRandomDice.sh
+++ Sequence/TwoRandomDice.sh
@@ -1,10 +1,8 @@
 #!/bin/bash -x
-#Add two random Dice number and print the result
 
-#taking two variables to store random dice numbers
+#TAKING TWO RANDOM NUMBERS
 randomDice1=$((RANDOM%6 + 1));
 randomDice2=$((RANDOM%6 + 1));
 
-#adding those numbers and printing the result
-add=$(($randomDice1 + $randomDice2));
-echo $add
+addition=$(($randomDice1 + $randomDice2));
+echo "Addition of two random numbers is=$addition"
