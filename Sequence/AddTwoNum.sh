322ed67412184bc0daadbc48832803a779861c35
vampire16 <siddheshsalvi525@gmai.com>
2020-02-22 15:16:53 +0530

    ADDED : Adding two numbers

--- Sequence/AddTwoNum.sh
+++ Sequence/AddTwoNum.sh
@@ -1,10 +1,9 @@
 #! /bin/bash -x
 
-#assign two numbers num1 and num2
-number1=100; 
-number2=200; 
+#CONSTANT VARIABLES
+number1=100;
+number2=200;
 
-#add two numbers and print addition
 addition=$(($number1 + $number2))
 echo "Addition of $number1 and $number2 is : $addition"
 
