c2a576a42386e411a4c8daae85795f84c00dcb64
vampire16 <siddheshsalvi525@gmai.com>
2020-02-22 18:27:44 +0530

    MODIFIED : added comments and double quotes

--- IfElse/FlipCoin.sh
+++ IfElse/FlipCoin.sh
@@ -1,12 +1,10 @@
 #!/bin/bash -x
-#write a program to simulate a coin flip and print out heads or tails
 
-#take random number
+#RANDOM NUMBER
 randomCheck=$((RANDOM%2))
-
-#check random number with specific number for heads 
+ 
 if [ $randomCheck -eq 0 ]
-then echo Heads
-else echo Tails
+then echo "Heads"
+else echo "Tails"
 fi
 
