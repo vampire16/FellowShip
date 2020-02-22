2671d42357125eaf8e33f105f1fd1ca8579781e3
vampire16 <siddheshsalvi525@gmai.com>
2020-02-22 15:54:37 +0530

    MODIFIED : added comments

--- Sequence/AddTwoNum.sh
+++ Sequence/AddTwoNum.sh
@@ -1,8 +1,8 @@
 #! /bin/bash -x
 
 #CONSTANT VARIABLES
-number1=100;
-number2=200;
+number1=101;
+number2=201;
 
 addition=$(($number1 + $number2))
 echo "Addition of $number1 and $number2 is : $addition"
