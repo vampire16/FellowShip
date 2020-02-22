dd90f92a9336c866788f613a9a20658b9468dfb3
vampire16 <siddheshsalvi525@gmai.com>
2020-02-22 16:57:17 +0530

    MODIFIED : added comments and changed variables name

--- Sequence/RectPlotMeter.sh
+++ Sequence/RectPlotMeter.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -x
 #Rectangular plot  of 60feet * 40feet in meters
+# 1 feet= 0.3048 meter
 
-#declaring two variables and assigning 60 and 40 values 
-val1=60
-val2=40
+#CONSTANT VARIABLES 
+value1=60
+value2=40
 
-meter=`echo "0.3048 * $val1 * $val2" | bc -l`      # 1 feet= 0.3048 meter
-echo $meter
+meter=`echo "0.3048 * $value1 * $value2" | bc -l`
+echo "Answer=$meter"
