0f6c03da3fe56e535681a696e09bd073c98f3f13
vampire16 <siddheshsalvi525@gmai.com>
2020-02-22 17:03:25 +0530

    MODIFIED : deleted commands and added double quotes

--- Sequence/RandomSingleDigit.sh
+++ Sequence/RandomSingleDigit.sh
@@ -1,7 +1,4 @@
 #!/bin/bash -x
 
-#Use Random function ((RANDOM)) to get Single Digit
-
-#store the output in variable and print the result
-randomCheck=$((RANDOM%10))          #((RANDOM)) it is a function
-echo $randomCheck
+randomCheck=$((RANDOM%10))
+echo "Single digit random number=$randomCheck"
